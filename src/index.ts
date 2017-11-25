709a03247949021dcd30389e59a52618c189c7b4
Kenichi Saito <hajime@studiohff.net>
2017-11-25 14:59:55 +0900

    ツイート内容をAPIで取得

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+import TwitterGateway from "./twitterGateway";
+
+const twitterGateway = new TwitterGateway({
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+});
+
+(async () => {
+    const tweet = await twitterGateway.getTweet("hajimepg", "934296995212156928");
+
+    console.log(JSON.stringify(tweet, null, 4));
+})()
+.catch(
+    (error) => console.log(error)
+);
---
+++ src/twitterGateway.ts
@@ -0,0 +1,33 @@
+import * as assert from "assert";
+
+import * as Twitter from "twitter";
+
+import Tweet from "./tweet";
+
+interface ITwitterCredential {
+    access_token_key: string | undefined;
+    access_token_secret: string | undefined;
+    consumer_key: string | undefined;
+    consumer_secret: string | undefined;
+}
+
+export default class TwitterGateway {
+    protected client;
+
+    constructor(credential: ITwitterCredential) {
+        this.client = new Twitter(credential);
+    }
+
+    public async getTweet(screenName: string, idStr: string): Promise<Tweet> {
+        return new Promise<Tweet>((resolve, reject) => {
+            this.client.get("statuses/show", { id: idStr }, (error, response: Tweet) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(response);
+            });
+        });
+    }
+}
